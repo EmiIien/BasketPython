7bf6c5f269afe003202761813aaa35b421d8b6cc
Jose <ravelomanantsoajose06@gmail.com>
2020-11-12 09:26:12 +0100

    Première version inscription/connexion/deconnexion

--- basket.py
+++ basket.py
@@ -2,11 +2,11 @@ import os
 import sqlite3
 import click
 
-from flask import current_app, g
+from flask import Flask,render_template,request,session,redirect,url_for,current_app,g
 from flask.cli import with_appcontext
-from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = 'testsecretkey'
 
 def get_db():
     if 'db' not in g:
@@ -42,3 +42,55 @@ def init_db_command():
 
 app.teardown_appcontext(close_db)
 app.cli.add_command(init_db_command)
+
+@app.route('/',methods={"GET","POST"})
+def login():
+    if request.method=="POST":
+        email=request.form['email']
+        pswd=request.form['pswd']
+        db = get_db()
+        mdp = db.execute('SELECT pswrd FROM user where mail = (?)',(email,)).fetchone()[0]
+        if(check_password_hash(mdp, pswd)):
+            session["email"]=email
+            return redirect(url_for('index'))
+        else:
+            return ("mdp ou mail incorrect")
+    else:
+        return render_template('login.html')
+
+
+@app.route('/index',methods={"get","post"})
+def index():
+    if "email" in session:
+        email=session["email"]
+        db=get_db()
+        users = (db.execute('SELECT * FROM user')).fetchall()
+        return render_template('index.html',mail=email,users = users)
+    else:
+        return render_template('login.html')    
+
+
+@app.route('/register',methods={"get","post"})
+def register():
+    if request.method=="POST":
+        username=request.form['username']
+        email=request.form['email']
+        age=request.form['age']
+        pswd=generate_password_hash(request.form['pswd'])
+        db=get_db()
+        testmail = db.execute('SELECT id_user FROM user WHERE mail = (?)',(email,)).fetchall()
+        if len(testmail)==0:
+            db = get_db()
+            db.execute('INSERT INTO user (username,pswrd,mail,age) VALUES(?,?,?,?)',(username,pswd,email,age))
+            db.commit()
+            return render_template('login.html')
+        else:
+            return ("mail déjà utilisé")
+    else:
+        return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop("email", None)
+    return render_template("login.html")
